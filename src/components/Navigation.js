ae5f48de80aa83b3a375358ef01a465186f7d893
Syed Baji Shaik <syedbs@langoor.com>
2020-09-19 16:33:50 +0530

    css fixes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,25 +5,25 @@ const Navigation = (props) => {
     console.log(active)
     return (
         <div className="col-lg-12 p-0 d-flex flex-row flex-lg-column flex-wrap justify-content-around align-items-center align-content-center py-0 py-lg-5 font-20 font-lg-24 h-100 nav-container">
-            <button className={`nav-iconn p-3 mb-3 mb-lg-0 bs-1 ${active === 'Home'?'active':''}`} title="Home" onClick={() => { props.onClickGotoComponentFn('Home') }}>
-                <span className="d-inline d-lg-none font-12 mr-2 mr-lg-0">Home</span>
-                <i className="fa fa-home"></i>
+            <button className={`nav-iconn p-0 px-3 mb-3 mb-lg-0 bs-1 ${active === 'Home'?'active':''}`} title="Home" onClick={() => { props.onClickGotoComponentFn('Home') }}>
+                <span className="font-14 mr-2">Home</span>
+                <i className="font-20 fa fa-home"></i>
             </button>
-            <button className={`nav-iconn p-3 mb-3 mb-lg-0 bs-1 ${active === 'About'?'active':''}`} title="About" onClick={() => { props.onClickGotoComponentFn('About') }}>
-                <span className="d-inline d-lg-none font-12 mr-2 mr-lg-0">About </span>
-                <i className="fa fa-user"></i>
+            <button className={`nav-iconn p-0 px-3 mb-3 mb-lg-0 bs-1 ${active === 'About'?'active':''}`} title="About" onClick={() => { props.onClickGotoComponentFn('About') }}>
+                <span className="font-14 mr-2">About </span>
+                <i className="font-20 fa fa-user"></i>
             </button>
-            <button className={`nav-iconn p-3 mb-3 mb-lg-0 bs-1 ${active === 'Resume'?'active':''}`} title="Resume" onClick={() => { props.onClickGotoComponentFn('Resume') }}>
-                <span className="d-inline d-lg-none font-12 mr-2 mr-lg-0">Resume </span>
-                <i className="fa fa-graduation-cap"></i>
+            <button className={`nav-iconn p-0 px-3 mb-3 mb-lg-0 bs-1 ${active === 'Resume'?'active':''}`} title="Resume" onClick={() => { props.onClickGotoComponentFn('Resume') }}>
+                <span className="font-14 mr-2">Resume </span>
+                <i className="font-20 fa fa-graduation-cap"></i>
             </button>
-            <button className={`nav-iconn p-3 mb-3 mb-lg-0 bs-1 ${active === 'Portfolio'?'active':''}`} title="Portfolio" onClick={() => { props.onClickGotoComponentFn('Portfolio') }}>
-                <span className="d-inline d-lg-none font-12 mr-2 mr-lg-0">Portfolio </span>
-                <i className="fa fa-suitcase font-18"></i>
+            <button className={`nav-iconn p-0 px-3 mb-3 mb-lg-0 bs-1 ${active === 'Portfolio'?'active':''}`} title="Portfolio" onClick={() => { props.onClickGotoComponentFn('Portfolio') }}>
+                <span className="font-14 mr-2">Portfolio </span>
+                <i className="font-20 fa fa-suitcase"></i>
             </button>
-            <button className={`nav-iconn p-3 bs-1 ${active === 'Contact'?'active':''}`} title="Contact" onClick={() => { props.onClickGotoComponentFn('Contact') }}>
-                <span className="d-inline d-lg-none font-12 mr-2 mr-lg-0">Contact </span>
-                <i className="fa fa-envelope font-18"></i>
+            <button className={`nav-iconn p-0 px-3 bs-1 ${active === 'Contact'?'active':''}`} title="Contact" onClick={() => { props.onClickGotoComponentFn('Contact') }}>
+                <span className="font-14 mr-2">Contact </span>
+                <i className="font-20 fa fa-envelope"></i>
             </button>
         </div>
     );
